7d432f6dc309781fafa1d29a4880c3086b9b0167
Aleksey Zayachnikov <lekseff@gmail.com>
2022-03-19 00:06:45 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+import GameSavingLoader from './GameSavingLoader';
+
+(async () => {
+  try {
+    const data = await GameSavingLoader.load();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+})();
